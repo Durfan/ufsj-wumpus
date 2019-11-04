05823b4baa603ea0e8ab322d9396caf72be03210
VitorFreireV <32673249+VitorFreireV@users.noreply.github.com>
2019-11-03 23:46:15 -0300

    agent.h att

--- include/agent.h
+++ include/agent.h
@@ -6,8 +6,11 @@ Agent *iniAgent(void);
 void ifengine(Agent *agent, Sensor sensor, Know *aquad, int **know);
 void chkghost(Agent *agent, Know *aquad, int **know);
 void chktraps(Know *aquad, int **know);
-void leapofaith(Agent *agent, int **world);
+int boundary(int v);
+List *leapofaith(Agent *agent, Know *aquad ,int **world, List *stateList, List *route);
 void move(Agent *agent, int quad);
+int getStateGhost(Know *aquad);
+List *BSF(Know *aquad, Agent *agent, int target);
 
 int wasted(Agent *agent, Quad *wquad);
 int winner(Agent *agent, Know *aquad);
