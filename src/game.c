0cad71867457d64b76e31e1b461f99cfaf634edd
VitorFreireV <32673249+VitorFreireV@users.noreply.github.com>
2019-11-08 12:24:15 -0300

    Update game.c

--- src/game.c
+++ src/game.c
@@ -1,71 +1,151 @@
 #include "main.h"
 
-Texture2D *texassets(void) {
-	Texture2D *texture = malloc(TEXTR * sizeof(Texture2D));
-	if (texture == NULL) {
+
+Agent *iniAgent(void) {
+	Agent *agent = malloc(sizeof(Agent));
+	if (agent == NULL) {
 		perror(PROJECT_NAME);
 		exit(EXIT_FAILURE);
 	}
-	texture[0]  = LoadTexture("resources/gold.png");
-	texture[1]  = LoadTexture("resources/agent.png");
-	texture[2]  = LoadTexture("resources/ghost.png");
-	texture[3]  = LoadTexture("resources/ghosd.png");
-	texture[4]  = LoadTexture("resources/traps.png");
-	texture[5]  = LoadTexture("resources/trapf.png");
-	texture[6]  = LoadTexture("resources/trapd.png");
-	texture[7]  = LoadTexture("resources/ewind.png");
-	texture[8]  = LoadTexture("resources/esmel.png");
-	texture[9]  = LoadTexture("resources/floor.png");
-	texture[10] = LoadTexture("resources/hwter.png");
-	texture[11] = LoadTexture("resources/lvbar.png");
-	texture[12] = LoadTexture("resources/ekill.png");
-	return texture;
+
+	agent->coord = START;
+	agent->lives = true;
+	agent->score = 0;
+	agent->ghost = 0;
+	agent->arrow = 1;
+	agent->grito = false;
+	agent->limit = false;
+
+	agent->state = explore;
+
+	return agent;
 }
 
-Rectangle *drawArray(int x) {
-	Rectangle *array = malloc(QUAD * sizeof(Rectangle));
-	if (array == NULL) {
-		perror(PROJECT_NAME);
-		exit(EXIT_FAILURE);
-	}
-	for (int i = 0; i < QUAD; i++) {
-		array[i].x =  x + 32*(i%WCOL); // separar: '+ px*(i%WCOL)'
-		array[i].y = 60 + 32*(i/WCOL);
-		array[i].width = 32;
-		array[i].height = 32;
-	}
-	return array;
-}
-
-void clrTxtres(Texture2D *texture) {
-	for (int i=0; i < TEXTR; i++)
-		UnloadTexture(texture[i]);
-	free(texture);
-}
-
-void manual(Agent *agent) {
+void ifengine(Agent *agent, Sensor sensor, Know *aquad, int **know) {
 	int coord = agent->coord;
+	aquad[coord].visit = true;
+	TriBol setinf;
+	bool visited;
 
-	if (IsKeyPressed(KEY_DOWN)) {
-		if (coord < (QUAD-WROW)) {
-			agent->coord += WROW;
-			agent->score -= 1;
+	if (agent->lives) {
+		aquad[coord].traps = nope;
+		aquad[coord].ghost = nope;
+	}
+
+	if (sensor.gold)
+		aquad[coord].gold = true;
+
+	if (sensor.smell) {
+		aquad[coord].smell = true;
+		if (!agent->ghost) {
+			for (int i=0; i < QUAD; i++) {
+				setinf  = aquad[i].ghost;
+				visited = aquad[i].visit;
+				if (know[coord][i] &&
+					(setinf == noinf) && !visited)
+					aquad[i].ghost = talvez;
+			}
 		}
-	} else if (IsKeyPressed(KEY_UP)) {
-		if (coord > 0) {
-			agent->coord -= WROW;
-			agent->score -= 1;
-		}
-	} else if (IsKeyPressed(KEY_RIGHT)) {
-		if (coord < (((coord/WROW)+1) * WCOL)-1) {
-			agent->coord += 1;
-			agent->score -= 1;
-		}
-	} else if (IsKeyPressed(KEY_LEFT)) {
-		if (coord > ((coord/WROW) * WCOL)) {
-			agent->coord -= 1;
-			agent->score -= 1;
+	}
+	else {
+		aquad[coord].smell = false;
+		if (!agent->ghost) {
+			for (int i=0; i < QUAD; i++)
+				if (know[coord][i])
+					aquad[i].ghost = nope;
 		}
 	}
 
+	if (sensor.wind) {
+		aquad[coord].wind = true;
+		for (int i=0; i < QUAD; i++) {
+			setinf  = aquad[i].traps;
+			visited = aquad[i].visit;
+			if (know[coord][i] &&
+				(setinf == noinf) && !visited)
+				aquad[i].traps = talvez;
+		}
+	}
+	else {
+		aquad[coord].wind = false;
+		for (int i=0; i < QUAD; i++) {
+			if (know[coord][i])
+				aquad[i].traps = nope;
+		}
+	}
+
+	if (!agent->ghost)
+		chkghost(agent,aquad,know);
+	chktraps(aquad,know);
+}
+
+void chkghost(Agent *agent, Know *aquad, int **know) {
+	bool found  = false;
+	TriBol ginfo;
+
+	for (int i=0; i < QUAD; i++) {
+		found = false;
+		ginfo = aquad[i].ghost;
+		if ((ginfo == talvez) && (getVgrau(know,i) >= 2)) {
+			for (int j=0; j < QUAD; j++) {
+				if (know[i][j] && !aquad[j].smell)
+					found = false;
+				else found = true;
+			}
+		}
+		if (found) {
+			agent->ghost = i;
+			aquad[i].ghost = certeza;
+		}
+	}
+
+	if (agent->ghost)
+		for (int i=0; i < QUAD; i++) {
+			ginfo = aquad[i].ghost;
+			if (ginfo == talvez || ginfo == noinf)
+				aquad[i].ghost = nope;
+	}
+}
+
+void chktraps(Know *aquad, int **know) {
+	bool found  = false;
+	TriBol ginfo;
+
+	for (int i=0; i < QUAD; i++) {
+		found = false;
+		ginfo = aquad[i].traps;
+		if ((ginfo == talvez) && (getVgrau(know,i) >= 3)) {
+			for (int j=0; j < QUAD; j++) {
+				if (know[i][j] && !aquad[j].wind)
+					found = false;
+				else found = true;
+			}
+		}
+		if (found)
+			aquad[i].traps = certeza;
+	}
+}
+
+void move(Agent *agent, int quad) {
+	agent->score -= 1;
+	agent->coord = quad;
+}
+
+int wasted(Agent *agent, Know *aquad, Quad *wquad) {
+	int pos = agent->coord;
+	if (wquad[pos].ghost) {
+		aquad[pos].ghost = 1;
+		return 1;
+	}
+	if (wquad[pos].traps) {
+		aquad[pos].traps = 1;
+		return 1;
+	}
+	return 0;
+}
+
+int winner(Agent *agent, Know *aquad) {
+	int pos = agent->coord;
+	if (aquad[pos].gold) return 1;
+	return 0;
 }
