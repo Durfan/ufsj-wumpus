5393d9ec0c04fefd78dc479a5ff39c3e31ccd0f0
VitorFreireV <32673249+VitorFreireV@users.noreply.github.com>
2019-11-08 12:27:18 -0300

    Update vitor.h

--- include/vitor.h
+++ include/vitor.h
@@ -1,25 +1,31 @@
 #ifndef INCLUDE_VITOR_H
 #define INCLUDE_VITOR_H
 
-void iniSttes(List *list);
-List *omgigoingtodie(Agent *agent, Know *aquad, int **world);
-List *BSF(Know *aquad, Agent *agent, int target);
-void adjNKnow(List *list, Know *aquad, int coord, int *visited, int *parents);
-void addVNode(List *list, Know *aquad, int state, int *visited, int *parents, int parent);
-void addAdjLt(List *list, int coord);
-void genRoute(List *route, int *parents, int init, int target);
-int bestSmell(Know *aquad);
-void CrazyMode(Know *aquad, List *stateList, int *target);
-void findVTargets(Agent *agent, Know *aquad, List *stateList);
+
+int ArrowMoviment(Quad *wquad, int coord, int target, int sum, Know *aquad, int t);
+int AtackEffect(Agent *agent, int target, Quad *wquad, Know *aquad);
+int choiceInsecureTarget(Know *aquad);
+int distStates(int x, int y);
 int findPosKillGhost(Agent *agent, Know *aquad, int target);
 int getStateGhost(Know *aquad);
 int bestMaybGhost(Know *aquad, int *posGhost);
-int ArrowMoviment(Quad *wquad, int coord, int target, int sum, Know *aquad, int t);
-int killWithAtack(Agent *agent, int target, Quad *wquad, Know *aquad);
-int distStates(int x, int y);
+int choiceSafeTarget(Agent *agent, Know *aquad);
+int choiceGhostTarget(Know *aquad);
+void agentAtack(Agent *agent, Quad *wquad, Know* aquad);
+void genRoute(List *route, int *parents, int init, int target);
+List *BSF(Know *aquad, Agent *agent, int target);
+bool xIsAdj(Agent *agent, int x);
+void addVNodeTail(List *list, Know *aquad, int state, int *visited, int *parents, int parent);
+void addAdjLt(List *list, int coord);
+void adjNKnow(List *list, Know *aquad, int coord, int *visited, int *parents);
+int bestTrapForDie(Know *aquad);
+
+
+bool killerMode(Agent *agent, Know *aquad, Quad *wquad, List *stateList);
+bool gotoxMode(Agent *agent, Know *aquad, Quad *wquad, List *stateList);
+bool gotodieMode(Agent *agent, Know *aquad, List *stateList);
+bool exploreMode(Agent *agent, Know *aquad, List *stateList);
 void vitor(Agent *agent, Know *aquad, List *stateList, Quad *wquad);
-void vitor2(Agent *agent, Know *aquad, List *stateList, Quad *wquad);
-bool exploreMode(Agent *agent, Know *aquad,List *stateList, Quad *wquad);
 
 //#pragma message __FILE__
 #endif // INCLUDE_VITOR_H
