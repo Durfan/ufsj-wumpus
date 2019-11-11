9d1c6463c655b2d706b29d9fc74309c1cca27322
VitorFreireV <32673249+VitorFreireV@users.noreply.github.com>
2019-11-11 15:19:26 -0300

    Update agent.c

--- src/agent.c
+++ src/agent.c
@@ -1,151 +1,202 @@
 #include "main.h"
 
+int main(void) {
 
-Agent *iniAgent(void) {
-	Agent *agent = malloc(sizeof(Agent));
-	if (agent == NULL) {
-		perror(PROJECT_NAME);
-		exit(EXIT_FAILURE);
-	}
+	setlocale(LC_ALL,"");
+	//SetConfigFlags(FLAG_SHOW_LOGO);
 
-	agent->coord = START;
-	agent->lives = true;
-	agent->score = 0;
-	agent->ghost = 0;
-	agent->arrow = 1;
-	agent->grito = false;
-	agent->limit = false;
+	InitWindow(320,240,"World of Wumpus Reloaded");
+	InitAudioDevice();
 
-	agent->state = explore;
+	Image icon = LoadImage("resources/icon.png");
+	SetWindowIcon(icon);
 
-	return agent;
-}
+	Music music = LoadMusicStream("resources/snd_mgear.ogg");
+	Sound dead  = LoadSound("resources/snd_scream.ogg");
+	PlayMusicStream(music);
 
-void ifengine(Agent *agent, Sensor sensor, Know *aquad, int **know) {
-	int coord = agent->coord;
-	aquad[coord].visit = true;
-	TriBol setinf;
-	bool visited;
+	Vector2 position;
+	Texture2D *texture = texassets();
 
-	if (agent->lives) {
-		aquad[coord].traps = nope;
-		aquad[coord].ghost = nope;
-	}
+	Rectangle frmRecS = {   0, 0, 32, 32 };
+	Rectangle frmRecO = {   0, 0, 32, 32 };
+	//Rectangle frmRecK = {   0, 0, 48, 48 };
+	Rectangle wborder = {  20,60,128,128 };
+	Rectangle aborder = { 168,60,128,128 };
 
-	if (sensor.gold)
-		aquad[coord].gold = true;
+	Rectangle *aquads = drawArray(20);
+	Rectangle *wquads = drawArray(168);
 
-	if (sensor.smell) {
-		aquad[coord].smell = true;
-		if (!agent->ghost) {
-			for (int i=0; i < QUAD; i++) {
-				setinf  = aquad[i].ghost;
-				visited = aquad[i].visit;
-				if (know[coord][i] &&
-					(setinf == noinf) && !visited)
-					aquad[i].ghost = talvez;
+	int currentFrame = 0;
+	int framesCounter = 0;
+	int framesSpeed = 8; // Number of spritesheet frames shown by second
+	SetTargetFPS(60);
+
+	Sensor sensor;
+	Agent *agent = iniAgent();
+	Quad *wquad = iniQuad();
+	Know *aquad = iniKnow();
+	int **world = alocArray(QUAD,QUAD);
+	int **know  = alocArray(QUAD,QUAD);
+	iniGraph(world);
+	genWorld(world,wquad);
+
+	int posagent;
+	float time = 0.0;
+
+	#ifdef DEBUG
+	prtGraph(world);
+	prtAdjac(world);
+	#endif
+
+	List *routeList = iniLst();
+
+	// Main game loop
+	while (!WindowShouldClose()) {
+		// Update variables
+		UpdateMusicStream(music);
+
+		if (agent->lives)
+			time += GetFrameTime();
+
+		framesCounter++;
+
+		if (framesCounter >= (60/framesSpeed)) {
+			framesCounter = 0;
+			currentFrame++;
+			if (currentFrame > 5)
+				currentFrame = 0;
+			frmRecS.x = currentFrame * 32;
+			//frmRecK.x = currentFrame * 48;
+			frmRecO.x = (currentFrame/2) * 32;
+		}
+
+		posagent = scanCord(agent);
+
+		if (wasted(agent,aquad,wquad)) {
+			StopMusicStream(music);
+			DrawText("WASTED!",100,45,10,RED);
+			framesCounter = 0;
+			if (agent->lives) {
+				agent->lives = false;
+				agent->score -= 1000;
+				PlaySound(dead);
+			}
+		} else if (winner(agent,aquad)) {
+			StopMusicStream(music);
+			DrawText("WINNER!",100,45,10,ORANGE);
+			framesCounter = 0;
+			if (agent->lives) {
+				agent->lives = false;
+				agent->score += 1000;
 			}
 		}
-	}
-	else {
-		aquad[coord].smell = false;
-		if (!agent->ghost) {
-			for (int i=0; i < QUAD; i++)
-				if (know[coord][i])
-					aquad[i].ghost = nope;
+
+		sensor = scanQuad(agent,wquad);
+		scanPath(agent,world,know,aquad);
+		scanLimt(agent,aquad);
+		ifengine(agent,sensor,aquad,know);
+
+		if (IsKeyPressed(KEY_R)) {
+			time = 0;
+			rstWorld(world,know,agent,wquad,aquad);
+			clrLst(routeList);
+			routeList = iniLst();
+			PlayMusicStream(music);
 		}
-	}
 
-	if (sensor.wind) {
-		aquad[coord].wind = true;
-		for (int i=0; i < QUAD; i++) {
-			setinf  = aquad[i].traps;
-			visited = aquad[i].visit;
-			if (know[coord][i] &&
-				(setinf == noinf) && !visited)
-				aquad[i].traps = talvez;
-		}
-	}
-	else {
-		aquad[coord].wind = false;
-		for (int i=0; i < QUAD; i++) {
-			if (know[coord][i])
-				aquad[i].traps = nope;
-		}
-	}
+		if (IsKeyPressed(KEY_SPACE))
+			vitor(agent,aquad,routeList,wquad);
 
-	if (!agent->ghost)
-		chkghost(agent,aquad,know);
-	chktraps(aquad,know);
-}
 
-void chkghost(Agent *agent, Know *aquad, int **know) {
-	bool found  = false;
-	TriBol ginfo;
+		manual(agent);
 
-	for (int i=0; i < QUAD; i++) {
-		found = false;
-		ginfo = aquad[i].ghost;
-		if ((ginfo == talvez) && (getVgrau(know,i) >= 2)) {
-			for (int j=0; j < QUAD; j++) {
-				if (know[i][j] && !aquad[j].smell)
-					found = false;
-				else found = true;
+		// Draw
+		BeginDrawing();
+
+			ClearBackground(RAYWHITE);
+
+			DrawText(FormatText("%05d", agent->score),20,35,20,DARKGRAY);
+			DrawTexture(texture[11],20,15,WHITE);
+			DrawTexture(texture[10],75,15,WHITE);
+			DrawText(FormatText("x %d", agent->arrow),92,20,10,DARKGRAY);
+			DrawText("Aperte r para resetar",20,205,10,DARKGRAY);
+			DrawText("Aperte [spacebar] para avançar",20,215,10,DARKGRAY);
+
+			DrawText(FormatText("Q%02d L%02d A%02d G%02d W%02d",
+				agent->coord, agent->limit, agent->arrow,
+				agent->grito, agent->ghost),
+				180,15,10,DARKGRAY);
+			DrawText(FormatText("W%02d S%02d G%02d P%02d",
+				sensor.wind, sensor.smell, sensor.gold, aquad[posagent].paths),
+				180,25,10,DARKGRAY);
+			DrawText(FormatText("T %02.02f",time),180,35,10,DARKGRAY);
+
+			showInfos(sensor);
+
+			for (int i = 0; i < QUAD; i++) {
+				DrawTexture(texture[9],aquads[i].x,aquads[i].y,WHITE);
+				position.x = aquads[posagent].x;
+				position.y = aquads[posagent].y;
+				DrawTextureRec(texture[1],frmRecS,position,WHITE);
+				if (aquad[i].visit) {
+					position.x = aquads[i].x + 16;
+					position.y = aquads[i].y + 16;
+					DrawCircleV(position,3,DARKGRAY);
+				}
+				position.x = aquads[i].x;
+				position.y = aquads[i].y;
+				if (aquad[i].gold)
+					DrawTextureRec(texture[0],frmRecO,position,WHITE);
+				if (aquad[i].wind)
+					DrawTextureRec(texture[7],frmRecO,position,WHITE);
+				if (aquad[i].smell)
+					DrawTextureRec(texture[8],frmRecO,position,WHITE);
+				if (aquad[i].ghost == talvez)
+					DrawTexture(texture[3],aquads[i].x,aquads[i].y,WHITE);
+				if (aquad[i].ghost == certeza)
+					DrawTextureRec(texture[2],frmRecS,position,WHITE);
+				if (aquad[i].traps == talvez)
+					DrawTexture(texture[6],aquads[i].x,aquads[i].y,WHITE);
+				if (aquad[i].traps == certeza)
+					DrawTexture(texture[5],aquads[i].x,aquads[i].y,WHITE);
 			}
-		}
-		if (found) {
-			agent->ghost = i;
-			aquad[i].ghost = certeza;
-		}
-	}
 
-	if (agent->ghost)
-		for (int i=0; i < QUAD; i++) {
-			ginfo = aquad[i].ghost;
-			if (ginfo == talvez || ginfo == noinf)
-				aquad[i].ghost = nope;
-	}
-}
-
-void chktraps(Know *aquad, int **know) {
-	bool found  = false;
-	TriBol ginfo;
-
-	for (int i=0; i < QUAD; i++) {
-		found = false;
-		ginfo = aquad[i].traps;
-		if ((ginfo == talvez) && (getVgrau(know,i) >= 3)) {
-			for (int j=0; j < QUAD; j++) {
-				if (know[i][j] && !aquad[j].wind)
-					found = false;
-				else found = true;
+			for (int i = 0; i < QUAD; i++) {
+				DrawTexture(texture[9],wquads[i].x,wquads[i].y,WHITE);
+				position.x = wquads[posagent].x;
+				position.y = wquads[posagent].y;
+				DrawTextureRec(texture[1],frmRecS,position,WHITE);
+				position.x = wquads[i].x;
+				position.y = wquads[i].y;
+				if (wquad[i].gold)
+					DrawTextureRec(texture[0],frmRecO,position,WHITE);
+				if (wquad[i].traps)
+					DrawTextureRec(texture[4],frmRecO,position,WHITE);
+				if (wquad[i].ghost)
+					DrawTextureRec(texture[2],frmRecS,position,WHITE);
 			}
-		}
-		if (found)
-			aquad[i].traps = certeza;
-	}
-}
 
-void move(Agent *agent, int quad) {
-	if(agent->coord != quad) agent->score -= 1;
-	agent->coord = quad;
-}
+			DrawRectangleLinesEx(wborder,2,DARKGRAY);
+			DrawRectangleLinesEx(aborder,2,DARKGRAY);
 
-int wasted(Agent *agent, Know *aquad, Quad *wquad) {
-	int pos = agent->coord;
-	if (wquad[pos].ghost) {
-		aquad[pos].ghost = 1;
-		return 1;
+		EndDrawing();
 	}
-	if (wquad[pos].traps) {
-		aquad[pos].traps = 1;
-		return 1;
-	}
-	return 0;
-}
 
-int winner(Agent *agent, Know *aquad) {
-	int pos = agent->coord;
-	if (aquad[pos].gold) return 1;
-	return 0;
+	// De-Initialization
+	freeArray(QUAD,world);
+	freeArray(QUAD,know);
+	clrLst(routeList);
+	free(wquad);
+	free(wquads);
+	free(aquad);
+	free(aquads);
+	free(agent);
+
+	clrTxtres(texture);
+	UnloadSound(dead);
+	CloseAudioDevice();
+	CloseWindow();
+
+	return EXIT_SUCCESS;
 }
